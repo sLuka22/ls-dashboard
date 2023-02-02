656f61180e2cf71e6ed6ee8a7e91aa745d3e4f0e
sLuka22 <94962084+sLuka22@users.noreply.github.com>
2023-02-02 18:56:29 +0100

    Add navigation bar (#2)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react"
 
+//Icons
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import LanguageIcon from '@mui/icons-material/Language';
+
 const Navbar = () => {
   return (
-    <header className="navbar">
-      Navbar
+    <header className="navbar border--bottom flex flex--middle">
+      <div className="navbar__wrapper flex flex--apart flex--middle">
+        <div className="navbar__search flex flex--middle border">
+          <input type="text" placeholder="Search..." />
+          <ManageSearchIcon className="icon icon--s icon--blue" />
+        </div>
+        <div className="navbar__icons  flex flex--middle">
+          <LanguageIcon className="icon icon--s icon--blue m-left--s" />
+          <GitHubIcon className="icon icon--s icon--blue m-left--s" />
+          <LinkedInIcon className="icon icon--s icon--blue m-left--s" />
+        </div>
+      </div>
     </header>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import Widget from "../components/Widget";
 
 const Home = () => {
     return (
         <main className="main flex bg--default">
           <Sidebar />
-          <div className="homepage home-page__container flex__item flex__item--hexa">Container</div>
+          <div className="homepage home-page__container flex__item flex__item--hexa">
+            <Navbar />
+            <div className="widgets">
+              <Widget />
+              <Widget />
+              <Widget />
+              <Widget />
+            </div>
+          </div>
         </main>
     )
 }
